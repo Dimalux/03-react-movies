663c1ccfc522b640190e426d09a160280a592d3b
Dima_V <astrallux777@ukr.net>
2025-07-29 20:30:38 +0300

    29.07.2025_R3-04

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,9 @@
-// import { createRoot } from "react-dom/client";
-// import App from "./components/App/App";
-// import "./index.css";
-
-// createRoot(document.getElementById("root") as HTMLDivElement).render(<App />);
-
-
-
-
-
-
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Toaster } from 'react-hot-toast';
 import App from './components/App/App';
 import 'modern-normalize/modern-normalize.css';
-import './index.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
