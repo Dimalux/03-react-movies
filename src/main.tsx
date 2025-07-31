704722d471d5be9fa3ebd4ddca74344d96e39279
Dima_V <astrallux777@ukr.net>
2025-07-31 20:20:54 +0300

    31.07.2025_R3-09

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,8 @@ import 'modern-normalize/modern-normalize.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <App />
-        <Toaster position="top-right" /> {/* Компонент для сповіщень */}
-    </React.StrictMode>
+   <React.StrictMode>
+    <Toaster position="top-center" />
+    <App />
+  </React.StrictMode>
 );
